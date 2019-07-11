28ed1c1b38aeb070298e7e41f7ad00b758c47677
David Jermaine Whitehead <david_whitehead_234@yahoo.com>
2019-07-11 23:33:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,32 +3,31 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_arry = ["Tom", "Paul", "Jack", "David"]
+  create_an_array = ["Tom", "Paul", "Jack", "David"]
 end
 
-def add_element_to_end_of_array(add_element_to_end_of_array, "11")
-  add_element_to_end_of_array = ["5", "7", "9"]
-  add_element_to_end_of_array.push("11")
+def add_element_to_end_of_array(array, element)
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = [index_number] 
 end
-
-def retrieve_first_element_from_array(array)
   
+def retrieve_first_element_from_array(array)
+  array = [in]
 end
 
 def retrieve_last_element_from_array(array)
